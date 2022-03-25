ec682e55b1dd59bdaedeb22555486588d6192f40
Archana <archanaarchu41033@gmail.com>
2022-03-25 19:23:03 +0530

    optimized imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-import 'dart:async';
 
-import 'package:cstask/controller/controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:internet_connection_checker/internet_connection_checker.dart';
 
-import 'view/imageView.dart';
 import 'view/list_images.dart';
 
 void main() async{
